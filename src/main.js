6d249cf16cec495750020420a1b4e6c2320d9b6a
spymb <spym1933@foxmail.com>
2022-07-14 21:40:09 +0800

    bug修复

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import string from './css.js'
-import string2 from './style.js'
+import string from './css.js';
+import string2 from './style.js';
 
-$('#buttons').after($(string2))
+$('#buttons').after($(string2));
 
 const player = {
   id: undefined,
@@ -21,58 +21,58 @@ const player = {
   n: 1,
 
   init: () => {
-    player.bindEvents()
-    player.play()
+    player.bindEvents();
+    player.play();
   },
 
   bindEvents: () => {
     for (let key in player.events) {
       if (player.events.hasOwnProperty(key)) {
-        const value = player.events[key] // pause / play / slow
-        document.querySelector(key).onclick = player[value]
+        const value = player.events[key]; // pause / play / slow
+        document.querySelector(key).onclick = player[value];
       }
     }
 
   },
 
   run: () => {
-    player.n += 1
+    player.n += 1;
     if (player.n > string.length) {
-      window.clearInterval(player.id)
-      return
+      window.clearInterval(player.id);
+      return;
     }
-    player.ui.demo.innerText = string.substr(0, player.n)
-    player.ui.demo2.innerHTML = string.substr(0, player.n)
-    player.ui.demo.scrollTop = player.ui.demo.scrollHeight
+    player.ui.demo.innerText = string.substr(0, player.n);
+    player.ui.demo2.innerHTML = string.substr(0, player.n);
+    player.ui.demo.scrollTop = player.ui.demo.scrollHeight;
   },
 
   play: () => {
-    window.clearInterval(player.id)
-    player.id = setInterval(player.run, player.time)
+    window.clearInterval(player.id);
+    player.id = setInterval(player.run, player.time);
   },
   pause: () => {
-    window.clearInterval(player.id)
+    window.clearInterval(player.id);
   },
   slow: () => {
-    player.pause()
-    player.time = 300
-    player.play()
+    player.pause();
+    player.time = 300;
+    player.play();
   },
   normal: () => {
-    player.pause()
-    player.time = 100
-    player.play()
+    player.pause();
+    player.time = 100;
+    player.play();
   },
   fast: () => {
-    player.pause()
-    player.time = 0
-    player.play()
+    player.pause();
+    player.time = 0;
+    player.play();
   },
   stop: () => {
-    player.pause()
-    player.ui.demo.innerText = string
-    player.ui.demo2.innerHTML = string
+    player.pause();
+    player.ui.demo.innerText = string;
+    player.ui.demo2.innerHTML = string;
   }
-}
+};
 
-player.init()
+player.init();
